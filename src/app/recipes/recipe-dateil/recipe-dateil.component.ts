8bfabc180f7faf8d686a23b43ee98eed9c310aaa
mzajac <matzaj101@gmail.com>
2018-08-18 09:37:38 +0200

    component structure

---
+++ src/app/recipes/recipe-dateil/recipe-dateil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-dateil',
+  templateUrl: './recipe-dateil.component.html',
+  styleUrls: ['./recipe-dateil.component.css']
+})
+export class RecipeDateilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
